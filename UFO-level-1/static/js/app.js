f273c433eda3c17f946294f1091e24be4968e498
Dan K <64311306+not4ewe@users.noreply.github.com>
2020-08-27 19:37:30 -0400

    tweaking

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -5,7 +5,6 @@ var tableData = data;
 var tbody = d3.select("tbody");
 var button = d3.select("#filter-btn");
 var inputField1 = d3.select("#datetime");
-//var inputField2 = d3.select("#city");
 var resetbtn = d3.select("#reset-btn");
 var columns = ["datetime", "city", "state", "country", "shape", "durationMinutes", "comments"]
 
@@ -25,14 +24,10 @@ populate(data);
 button.on("click", () => {
 	d3.event.preventDefault();
 	var inputDate = inputField1.property("value").trim();
-	///var inputCity = inputField2.property("value").toLowerCase().trim();
+
 	// Filter by field matching input value
 	var filterDate = data.filter(data => data.datetime === inputDate);
 	console.log(filterDate)
-	///var filterCity = data.filter(data => data.city === inputCity);
-	///console.log(filterCity)
-	///var filterData = data.filter(data => data.datetime === inputDate ///&& data.city === inputCity);
-	///console.log(filterData)
 
 	// Add filtered sighting to table
 	tbody.html("");
@@ -44,10 +39,7 @@ button.on("click", () => {
 	if (response.filterDate.length !== 0) {
 		populate(filterDate);
 	}
-		//else if (response.filterDate.length === 0 ){
-		//	populate(filterDate);
 	
-		//}
 		else {
 			tbody.append("tr").append("td").text("No results found!"); 
 		}
